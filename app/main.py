6d3a982af2d64387c8993e1a0af3e87c63e58b74
Akshaya <akshaya8804@gmail.com>
2026-02-24 10:41:48 +0530

    All new changes

--- app/main.py
+++ app/main.py
@@ -49,6 +49,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8000",
         "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:8000",
         "https://india-estuaries-api.onrender.com",
     ],
     allow_credentials=True,
